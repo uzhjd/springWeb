0b5ebd9ba34fce5cab909b14a845228adcdabbf6
uzhjd <73466440+uzhjd@users.noreply.github.com>
2022-01-10 13:50:12 +0900

    chap.5

--- src/main/java/com/web/BootWebApplication.java
+++ src/main/java/com/web/BootWebApplication.java
@@ -5,24 +5,37 @@ import com.web.springbootcommunityweb.domain.User;
 import com.web.springbootcommunityweb.domain.enums.BoardType;
 import com.web.springbootcommunityweb.repository.BoardRepository;
 import com.web.springbootcommunityweb.repository.UserRepository;
+import com.web.springbootcommunityweb.resolver.UserArgumentResolver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
-public class BootWebApplication {
+public class BootWebApplication extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private UserArgumentResolver userArgumentResolver;
 
     public static void main(String[] args) {
         SpringApplication.run(BootWebApplication.class, args);
     }
 
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(userArgumentResolver);
+    }
+
     @Bean
-    public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) throws Exception {
+    public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) {
         return (args) -> {
             User user = userRepository.save(User.builder()
                     .name("havi")
